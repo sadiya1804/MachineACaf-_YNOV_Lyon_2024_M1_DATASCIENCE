abba56b9ba3b7391090e489b9c07dd4e11dab28b
Mourad2511 <mourad.amoussa@epitech.eu>
2025-01-01 23:15:31 +0100

    [GREEN]: test for latte is ok

---
+++ src/extended_brewer_interface.py
@@ -0,0 +1,8 @@
+from hardware.brewer import BrewerInterface
+
+class ExtendedBrewerInterface(BrewerInterface):
+    def pour_latte(self) -> bool:
+        pass
+
+    def pour_capuccino(self) -> bool:
+        pass
--- src/machine_a_cafe.py
+++ src/machine_a_cafe.py
@@ -1,9 +1,10 @@
 from hardware.brewer import BrewerInterface
+from extended_brewer_interface import ExtendedBrewerInterface
 from hardware.creditcard import CreditCardInterface, CardHandleInterface
 from hardware.cupprovider import CupProviderInterface
 
 class MachineACafé:
-    def __init__(self, brewer: BrewerInterface, lecteur_cb: CreditCardInterface, cup_provider: CupProviderInterface = None,):
+    def __init__(self, brewer: ExtendedBrewerInterface, lecteur_cb: CreditCardInterface, cup_provider: CupProviderInterface = None,):
         lecteur_cb.register_card_detected_callback(self._credit_card_callback)
         self._brewer = brewer
         self._cup_provider = cup_provider
@@ -31,3 +32,15 @@ class MachineACafé:
         brewing_success = self._brewer.pour_chocolate()
         if not brewing_success:
             card_handle.refund(60)
+    
+    def _latte_callback(self, card_handle: CardHandleInterface) -> None:
+        if self._cup_provider and not self._cup_provider.is_cup_present():
+            self._cup_provider.provide_cup()
+
+        debit_success = card_handle.try_charge_amount(70)
+        if not debit_success:
+            return
+        
+        brewing_success = self._brewer.pour_latte()
+        if not brewing_success:
+            card_handle.refund(70)
